cf573049ab006d473884ed53f81b2af2f61e345f
Juriy Bura <juriy.bura@gmail.com>
2016-03-31 11:40:51 +0800

    Added image loading

--- src/main.js
+++ src/main.js
@@ -3,8 +3,31 @@
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 
+let playerImg;
+
+function loadImage(url, cb) {
+    let img = new Image();
+    img.src = url;
+    img.onload = () => {
+        cb(null, img);
+    };
+
+    img.onerror = (e) => {
+        cb(e);
+    }
+}
+
 function init() {
-    ctx.fillRect(10, 10, 50, 50);
+    loadImage('res/player-blue.png', (err, img) => {
+        playerImg = img;
+        drawScene();
+    });
+}
+
+function drawScene() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(playerImg, 100, 100);
 }
 
 init();
